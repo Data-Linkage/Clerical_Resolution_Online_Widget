ffdb01cd2c7ee47ad99a6b7942831cafd0542ff2
odairh <Hannah.O'Dair@ons.gov.uk>
2023-04-21 14:15:08 +0100

    initial and updates

--- flask_poc/flask_new_flow.py
+++ flask_poc/flask_new_flow.py
@@ -5,7 +5,8 @@ import logging
 from flask import Flask, render_template, request, redirect, \
 url_for, flash, make_response, session, jsonify
 import os
-
+import ast
+import numpy as np
 from dlh_utils import sessions
 from dlh_utils import utilities
 
@@ -16,18 +17,51 @@ logging.getLogger('werkzeug').disabled=True
 spark=sessions.getOrCreateSparkSession(appName='crow_test', size='medium')
 
 file=spark.sql('SELECT * FROM crow.test3')
-#utilities.write_format(dataframe,'hive' ,'crow', 'test2')
+
 
 #extract list of cluster ids and separate out into dataframes
 
 working_file=file.toPandas()
-working_file = working_file.sort_values(by = 'Cluster_Number').reset_index()
+#working_file = working_file.sort_values(by = 'Cluster_Number').reset_index().astype(str)
 
-#working_file['Match']=''
+#temp thing
+#fill nulls in match column with '[]'.This is just to enable the advance cluster function, as is to work
+#could probably be improved so we don't have to do this. 
+working_file['Match'].replace('nan', '[]',inplace=True)
+working_file
 #####working poc#######
 #######################
 
+##################
+##HELPER FUNCTIONS#####
+#Note will probably eventually move these into a separate script
+
+def advance_cluster():
+  #note this function is very clunky and could likely be improved. 
+  """
+  1)A Function to: determine the number of matches made in a given cluster
+  2)If 1 or 0 unmatcher records in cluster remain; progress to the next cluster. 
+  
+  Parameters: None
+  Returns: None
+  
+  """
+  num_in_cluster=len(working_file.loc[working_file['Cluster_Number']==str(session['index'])])
+  print(num_in_cluster)
+  list_decided=[set(ast.literal_eval(i)) for i in working_file.loc[working_file['Cluster_Number']==str(session['index'])]['Match']]
+  print(list_decided)
+  uni_set_decided={x for l in list_decided for x in l}
+  print(uni_set_decided)
+  num_decided=len(uni_set_decided)
+  if (num_in_cluster-num_decided)<=1:
+      for rec_id in [x for x in working_file.loc[working_file['Cluster_Number']==str(session['index'])]['Resident_ID'] if x not in uni_set_decided]: 
+            working_file.loc[working_file['Resident_ID']==rec_id,'Match']=f"['No Match In Cluster For {rec_id}']"
+      session['index'] = int(session['index'])+ 1
+    
+
+#######################
 app= Flask(__name__)
+#may need to be something more secretive/encryptable! 
 app.config['SECRET_KEY']='abcd'
 
 
@@ -41,7 +75,6 @@ def new_session():
     button = request.form.get("hdfs")
     config_status = request.form.get("config")
     version = request.form.get("version")
-    
 
     return render_template("new_session.html", button=button,
                                               version=version,
@@ -55,11 +88,6 @@ def load_session():
 
 @app.route('/cluster_version', methods=['GET','POST'])
 def index(): 
-#      working_file = request.files['input_file']
-#      working_file.save('/tmp/working_file.csv')
-#      pd.read_csv('/tmp/working_file.csv')
-#      working_file['Match']=''
-#      
       #set highlighter toggle to 0
       session['highlighter']=False
       if 'index' not in session:
@@ -68,30 +96,48 @@ def index():
       else: 
         pass
         #logic to display firdt unlinked record first. 
-
-      cluster = request.form.getlist("cluster")
+      
+      if {'Match'}.issubset(working_file.columns):
+        print('Matching_Began')
+            # variable indicates whether user has returned to this file (1) or not (0)
+        
+      else:
+            
+            # create a match column and fill with blanks
+          working_file['Match'] = ''
+       
+      #cluster = request.form.getlist("cluster")
+      #print(cluster)
       
       # df=working_file.loc[working_file['Cluster_Number']==session['index']]
       # session['index'] = int(session['index'])+ 1
-
+      #cur_indexes=list(working_file.loc[working_file['Cluster_Number']==session['index']].index.values)
       if request.form.get('Match')=="Match":
-              print(cluster)
-#              working_file['Match'] = working_file['Match'].iloc[cluster]['Match']=1
-#              working_file['Match'] = working_file['Match'].loc[(working_file['Cluster_Number']==session['index']) & (working_file['Match']!= 1)]=0
-              working_file.loc[working_file['Cluster_Number']==session['index'],'Match']=1
-              
-              session['index'] = int(session['index'])+ 1
+              cluster = request.form.getlist("cluster")
+              for i in cluster:
+                  #note resident ID will need to change from to be read from a config as any reccord id 
+                  working_file.loc[working_file['Resident_ID']==i,'Match']=str(cluster)
+              advance_cluster()
+            
 
       elif request.form.get('Non-Match')=="Non-Match":
-              working_file.loc[working_file['Cluster_Number']==session['index'],'Match']=0
-              session['index'] = int(session['index'])+ 1
+              #note this section needs building out. 
+              #working_file.loc[working_file['Cluster_Number']==session['index'],'Match']=0
+              cluster = request.form.getlist("cluster")
+              for i in cluster:
+                  #note resident ID will need to change from to be read from a config as any reccord id 
+                  working_file.loc[working_file['Resident_ID']==i,'Match']=f"['No Match In Cluster For {i}']"
+              advance_cluster()
+              
+              
+              
 
       if request.form.get('back')=="back":
               session['index'] = int(session['index'])-1
 
       if request.form.get('save')=="save":
             table=utilities.pandas_to_spark(working_file)
-            utilities.write_format(table,'hive' ,'crow', 'test2')
+            utilities.write_format(table,'hive' ,'crow', 'test3')
       
       if 'index' not in working_file.columns:
           index = (list(range(max(working_file.count()))))
@@ -99,17 +145,23 @@ def index():
       else:
           pass
 
-      df=working_file.loc[working_file['Cluster_Number']==session['index']]
-      df_index = list(df['index'].values)
+      df=working_file.loc[working_file['Cluster_Number']==str(session['index'])]
 
       
       columns = df.columns
-      data = zip(df.values, df_index)
+      data = df.values
 
       return  render_template("cluster_version.html",
                               data = data,
                               columns=columns)
 
+    
+    
+    
+    
+    
+    
+    
 @app.route('/pairwise_version', methods=['GET','POST'])
 def index_pairwise(): 
       #set highlighter toggle to 0
@@ -140,7 +192,8 @@ def index_pairwise():
 
       if request.form.get('save')=="save":
             table=utilities.pandas_to_spark(working_file)
-            utilities.write_format(table,'hive' ,'crow', 'test2')
+            print('save activated ')
+            utilities.write_format(table,'hive' ,'crow', 'test3')
 
 
 
