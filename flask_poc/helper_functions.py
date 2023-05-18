13d8cea4a60b917fbbc0bddfe5f16ff3cb872e9f
odairh <Hannah.O'Dair@ons.gov.uk>
2023-05-18 16:50:42 +0100

    moved some more functions

--- flask_poc/helper_functions.py
+++ flask_poc/helper_functions.py
@@ -33,3 +33,61 @@ def save_rename_hive(dataframe, old_path,new_path):
     spark.sql(f"""DROP TABLE IF EXISTS {old_path}""")
     #unhash to delete after new table created 
     spark.sql(f"""CREATE TABLE IF NOT EXISTS {new_path} AS SELECT * FROM temp_table""")
+
+
+def get_save_paths(origin_file_path,origin_file_path_fl ):
+      if 'inProgress' in origin_file_path_fl[-1]:
+
+            # If it is the same user
+            if (user in origin_file_path_fl[-1]):
+                # Dont rename the file
+                in_prog_path= origin_file_path
+                end_file_name=origin_file_path_fl[-1][:-11]+'_DONE'
+                # create the filepath name for when the file is finished
+                filepath_done = ".".join([origin_file_path_fl[0], end_file_name])
+                print(f'filepath done={filepath_done}')
+                print(f'in prog path={in_prog_path}')
+            else:
+
+                print('USER_NOT_IN_NAME')
+
+                # Rename the file to contain the additional user
+                in_prog_path = origin_file_path_fl[-1][:-11]+f'_{user}'+'_inProgress'
+
+
+                end_file_name=origin_file_path_fl[-1][:-11]+f'_{user}'+'_DONE'
+                filepath_done=".".join([origin_file_path_fl[0],end_file_name ])
+                print(f'filepath done={filepath_done}')
+                print(f'in prog path={in_prog_path}')                        
+
+
+
+        # If a user is picking this file again and its done
+      elif 'DONE' in origin_file_path_fl[-1]:
+
+            # If it is the same user
+            if (user in origin_file_path_fl[-1]):
+                # dont change filepath done - keep it as it is
+                filepath_done = origin_file_path
+
+                # Rename the file 
+                in_prog_path=".".join([origin_file_path_fl[0],origin_file_path_fl[-1][:-5]+'_inProgress'])
+                print(f'filepath done={filepath_done}')
+                print(f'in prog path={in_prog_path}')
+
+            else:
+                # If it is a different user
+                # Rename the file to include the additional user
+                in_prog_path=".".join([origin_file_path_fl[0],origin_file_path_fl[-1][:-5]+f'_{user}'+'_inProgress'])
+
+                # create the filepath done
+                filepath_done=".".join([origin_file_path_fl[0],in_prog_path[:-11]+'_DONE' ])
+                print(f'filepath done={filepath_done}')
+                print(f'in prog path={in_prog_path}')
+                
+      else:
+
+              in_prog_path=".".join([origin_file_path_fl[0],origin_file_path_fl[-1]+f'_{user}'+'_inProgress' ])
+              filepath_done=".".join([origin_file_path_fl[0],origin_file_path_fl[-1]+f'_{user}'+'_DONE' ])
+      
+      return in_prog_path, filepath_done
